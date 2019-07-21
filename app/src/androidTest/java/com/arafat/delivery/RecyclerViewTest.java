4a224bd0fccb145e88c2d673cc7ebd0f90bf16d7
Hossain Md. Arafat <arafat@Hossains-MacBook-Pro.local>
2019-07-21 17:46:54 +0600

    Recyclerview first item click test with espresso

---
+++ app/src/androidTest/java/com/arafat/delivery/RecyclerViewTest.java
@@ -0,0 +1,28 @@
+package com.arafat.delivery;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.action.ViewActions.click;
+
+
+public class RecyclerViewTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void recycleTest(){
+
+        Espresso.onView(ViewMatchers.withId(R.id.rvDeliveries))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+    }
+}
